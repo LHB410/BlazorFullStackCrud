70906c36f9b0ec766b0e0b27f1506301106bd441
Laura Brooks <laura.brooks0@outlook.com>
2022-04-26 15:56:19 +0900

    trying to debug app

--- BlazorFullStackCrud/Server/Controllers/SuperHeroController.cs
+++ BlazorFullStackCrud/Server/Controllers/SuperHeroController.cs
@@ -44,48 +44,16 @@ namespace BlazorFullstackCrud.Server.Controllers
     }
 
     [HttpGet("{id}")]
-  
+
     public async Task<ActionResult<List<Superhero>>> GetSingleHero(int id)
     {
         var hero = heroes.FirstOrDefault(h => h.Id == id);
         if (hero == null)
         {
-                return NotFound(hero);
+            return NotFound(hero);
         }
         return Ok(hero);
     }
-
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
 
